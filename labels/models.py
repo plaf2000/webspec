bb42726d60867a90ffef85669a48af01626f31cc
plaf2000 <plaf2000@gmail.com>
2020-08-26 00:57:10 +0200

    Labels and species environment created

--- labels/models.py
+++ labels/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-class Labels(models.Model):
-    name = models.CharField()
+class Label(models.Model):
+    name = models.CharField(max_length=100)
     description = models.TextField()
-    
+    species_id = models.PositiveIntegerField()
+
+class Species(models.Model):
+    code = models.CharField(max_length=6)
+    lat = models.CharField(max_length=100)
+    en = models.CharField(max_length=100)
+    it = models.CharField(max_length=100)
