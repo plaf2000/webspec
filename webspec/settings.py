b2f847641a012e12713e69c503bb558d4bfef916
plaf2000 <plaf2000@gmail.com>
2021-02-11 01:12:20 +0100

    .gitignore

--- webspec/settings.py
+++ webspec/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 'k+8ku6hcuw#0w^ui0+h61@aaf80z6-aj+yd#e-1p(nlex2e++c'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
