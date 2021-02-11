91eb2940c7d3f65b37f478f740d20eb2b73e76f3
plaf2000 <plaf2000@gmail.com>
2021-02-11 01:14:18 +0100

    Test

--- webspec/settings.py
+++ webspec/settings.py
@@ -22,6 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ['SECRET_KEY']
+#newline
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
