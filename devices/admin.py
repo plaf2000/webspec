38632d5d8c32b0577b3db34d81f7d8e1733fc75a
plaf2000 <plaf2000@gmail.com>
2021-06-09 01:28:41 +0200

    .gitignore recorrected.

--- devices/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Recorder, Device, DeviceContext
-
-
-admin.site.register(Recorder)
-admin.site.register(Device)
-admin.site.register(DeviceContext)
--- labels/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Label, Species
-
-admin.site.register(Label)
-admin.site.register(Species)
