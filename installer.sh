5aa7dd8f772cc5e3c1ab7a740025c2a36019952d
plaf2000 <plaf2000@gmail.com>
2021-09-20 16:07:37 +0200

    Saving key in file instead of environment variable

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-export SECRET_KEY="`python3 generatekey.py`"
+echo "`python3 generatekey.py`" > key
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
--- webspec/settings.py
+++ webspec/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['SECRET_KEY']
+with open('key','r') as key_file:
+    SECRET_KEY = key_file.read()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
