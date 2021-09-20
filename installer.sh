e2821f3f6577cd79c4a91597a6e567f638b63e75
plaf2000 <plaf2000@gmail.com>
2021-09-20 16:22:25 +0200

    installer.sh corrected.

--- installer.sh
+++ installer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 pip3 install -r requirements.txt
-echo "`python3 generatekey.py`" > key
+echo "`python3 generatekey.py`" > key.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 python3 manage.py createsuperuser
